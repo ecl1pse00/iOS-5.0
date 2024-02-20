615d077e5a93078931f312fcb76685c844280926
Ignashev Andrey <ignashev.2002@mail.ru>
2024-02-20 13:07:28 +0300

    Outputting personal information about myself to the console

---
+++ prog.swift
@@ -0,0 +1,9 @@
+let age = 21
+let firstName = "Andrey"
+let secondName = "Ignashev"
+let univ = "SPB ETU LETI"
+let workPlace = "NIC SPB ETU"
+let pole = "Backend"
+let language = "Java"
+
+print("Hello. My name is \(firstName) \(secondName). I am \(age) years old. I study at \(univ) and work at \(workPlace). I am interested in a \(language) programming language.")
